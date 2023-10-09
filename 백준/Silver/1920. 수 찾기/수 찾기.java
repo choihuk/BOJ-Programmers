d7e8f8fd8d0204ab2e71f8450ff2e01852a4d8f8
Hyeok_Choi <39454230+choihuk@users.noreply.github.com>
2023-10-10 00:00:01 +0900

    [Silver IV] Title: 수 찾기, Time: 1516 ms, Memory: 56232 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        Set<Integer> set = new HashSet<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N ; i++) {
+            set.add(Integer.parseInt(st.nextToken()));
+        }
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            int token = Integer.parseInt(st.nextToken());
+            if (set.contains(token)) {
+                System.out.println("1");
+            } else {
+                System.out.println("0");
+            }
+        }
+    }
+}
